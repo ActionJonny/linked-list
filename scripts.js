f0f88c08838e2a1538cf8660339a09323c9cdb53
Jon Willemain <jon.willemain@gmail.com>
2016-12-12 18:44:33 -0700

    we are bubbling!

--- scripts.js
+++ scripts.js
@@ -1,31 +1,26 @@
-var websiteTitleInput = document.querySelector('.website-title-input');
-var websiteUrlInput = document.querySelector('.website-url-input');
-var enterButton = document.querySelector('.enter-button');
-var websiteTitleBookmarked = document.querySelector('.website-title-bookmarked');
-var websiteUrlBookmarked = document.querySelector('.website-url-bookmarked');
-var readButton = document.querySelector('.read-button');
-var deleteButton = document.querySelector('.delete-button');
-var rightContainer = document.querySelector('.right-container');
-
-
-enterButton.addEventListener('click', function() {
-  websiteTitleBookmarked.innerText = websiteTitleInput.value;
-  websiteUrlBookmarked.innerText = websiteUrlInput.value;
-  myFunction();
-})
-
-function myFunction() {
-    var h = document.createElement("H1");
-    var t = document.createTextNode(websiteTitleInput.value);
-    h.appendChild(t);
-    rightContainer.appendChild(h);
+function createBookmark(bookmark) {
+  $('.right-container').append(
+    `<li class='bookmarked-container'>
+      <h3 class='website-title-bookmark'>${bookmark.title}</h3>
+      <h4 class='website-url-bookmark'>${bookmark.url}</h4>
+      <button class='read-button'>Read</button>
+      <button class='delete-button'>Delete</button>
+    </li>`
+  )
 }
 
-//
+function Bookmark(title, url){
+ this.title = title;
+ this.url = url;
+}
 
+$('.enter-button').on('click', function(){
+ var title = $('.website-title-input').val();
+ var url = $('.website-url-input').val();
+ var bookmark = new Bookmark(title, url);
+ createBookmark(bookmark);
+});
 
-
-// var newElement = document.createElement('div');
-// newElement.innerHTML = xmlhttp.responseText;
-// document.getElementById("some_id").appendChild(newElement);
-//
+$('.right-container').on('click', '.delete-button', function(){
+  $(this).parent().remove()
+})
